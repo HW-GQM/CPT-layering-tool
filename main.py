03a6108a50b446edb5cacb8a05f56f5e4b0dd125
HW-GQM <yu.wong@geoquip-marine.com>
2025-03-20 11:48:05 +0000

    3D plots initial commit
    Added 3D plot alongside other potential new features

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import scipy
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import io
@@ -21,17 +22,20 @@ st.set_page_config(
     page_icon=":pick:",  # Path to your favicon file
 )
 
-# ✅ Authenticate with Google Drive
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-SERVICE_ACCOUNT_FILE = st.secrets["google"]["service_account_json"]
-service_account_file = '/tmp/service_account.json'
+def google_auth():
+    # ✅ Authenticate with Google Drive
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-with open(service_account_file, 'w') as f:
-    f.write(SERVICE_ACCOUNT_FILE)
+    SERVICE_ACCOUNT_FILE = st.secrets["google"]["service_account_json"]
+    service_account_file = '/tmp/service_account.json'
 
-# Load credentials
-creds = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
-client = gspread.authorize(creds)
+    with open(service_account_file, 'w') as f:
+        f.write(SERVICE_ACCOUNT_FILE)
+        
+    # Load credentials
+    creds = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
+    client = gspread.authorize(creds)
+    return client
 
 # ✅ Specify the shared folder ID
 FOLDER_ID = "1S0xXeHz4AsPeIH6oWqJkGRHgZSfdTc8A"
@@ -48,6 +52,7 @@ def create_or_access_spreadsheet():
 # ✅ Log events into the spreadsheet
 @st.cache_data
 def log_event(message):
+    client = google_auth()
     spreadsheet = create_or_access_spreadsheet()
     
     # Get the first worksheet
@@ -71,17 +76,20 @@ def log_event(message):
 if 'user_id' not in st.session_state:
     st.session_state.user_id = str(uuid.uuid4())  # Generate a random UUID for the session
 
-log_event(f"User accessed the app")
+#log_event(f"User accessed the app")
 
 # App starts here ****************************************
 
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 2rem;
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
+                }
+                .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
+                font-size:1.2rem;
                 }
         </style>
         """, unsafe_allow_html=True)
@@ -90,7 +98,7 @@ st.markdown("""
 @st.cache_data
 def load_data():
     df = pd.read_excel("training data/EnBW training data_rev3.2_filtered.xlsx")
-    log_event(f"Data loaded")
+    #log_event(f"Data loaded")
     print("data loaded")
     return df
 
@@ -102,89 +110,154 @@ def convert_for_download(df):
 
 raw_df = load_data()
 
-st.sidebar.subheader("CPT Layer Interpretation Tool")
+tab1, tab2 = st.tabs(["Editor", "3D Model"])
 
-with st.sidebar.expander("📘 Instructions"):
-    st.markdown("""
-    ### 
-    1. **Step 1**: Select a borehole from the dropdown.
-    2. **Step 2**: Adjust layer boundaries by dragging the sliders. Fine-tune using the red dot with the left/right arrow keys.
-    3. **Step 3**: For every borehole, click "Save Data", enter your name, and download the data as a CSV.
-    4. **Step 4**: Save all CSV files to Z:\\06. TRANSFER\Hin W\CPT layering tool\csv data exports
-                
-    ❗ **Note**: Data will not be saved if the borehole is changed.
-    """)
+with tab1:
 
-bh = st.sidebar.selectbox("Borehole", raw_df["BH"].unique())
+    st.sidebar.subheader("CPT Layer Interpretation Tool")
 
-df = raw_df[raw_df["BH"] == bh]
+    with st.sidebar.expander("📘 Instructions"):
+        st.markdown("""
+        ### 
+        1. **Step 1**: Select a borehole from the dropdown.
+        2. **Step 2**: Adjust layer boundaries by dragging the sliders. Fine-tune using the red dot with the left/right arrow keys.
+        3. **Step 3**: For every borehole, click "Save Data", enter your name, and download the data as a CSV.
+        4. **Step 4**: Save all CSV files to Z:\\06. TRANSFER\Hin W\CPT layering tool\csv data exports
+                    
+        ❗ **Note**: Data will not be saved if the borehole is changed.
+        """)
 
-num_layers = st.sidebar.number_input("Number of layers:", min_value=1, max_value=10, value=3, step=1)
-
-layers = []
-for i in range(num_layers - 1):
-    # User input (e.g., depth range selection)
-    depth = st.sidebar.slider(f"Layer boundary {i+1}", 0.0, df["Depth (m)"].max(), 1.0, step=0.1, format="%0.1f", key=i)
-    layers.append(depth)
-
-@st.dialog("Input your name:")
-def save_data():
-    user_name = st.text_input("Enter your name:")
-    #if st.button("Submit"):
-    if user_name:
-        # Build dataframe
-        layer = layers.sort()    # Make sure the layers are in order
-        save_df = pd.DataFrame(layers, columns=[bh])
-        csv_data = convert_for_download(save_df)
-        download = st.download_button("Download CSV", data=csv_data, file_name=f"{bh}_cpt layering_{user_name}.csv", mime="text/csv")
-        if download:
-            log_event(f"Data downloaded : {bh} {user_name}")
-            st.success(f"Data downloaded :)")
-    else:
-        st.error("Please enter your name.")
-
-# User clicks Save Button
-if st.sidebar.button("Save Data"):
-    save_data()
-
-params = ["Qt", "Fr", "Bq", "Ic"]
-
-fig = make_subplots(
-rows=1, cols=4,          # Number of rows and columns
-shared_yaxes=True,       # Share x-axis for all subplots
-vertical_spacing=0.1,    # Spacing between rows
-subplot_titles=(["Qt [kPa]", "Fr [-]", "Bq [-]", "Ic [-]"])  # Titles for each subplot
+    # Select parammeter
+    with st.sidebar.expander("Options"):
+        selected_params = st.multiselect(
+        "Select exactly 4 parameters", 
+        options=["Qt", "Fr", "Bq", "Ic"], 
+        default=[], 
+        max_selections=2  # Optionally limit to 4 selections
 )
 
-# Plot Ic zones
-fig.add_shape(type="rect", x0=0, x1=1.31, y0=0, y1=df["Depth (m)"].max(), fillcolor="crimson", opacity=0.2,
-                layer="below", row=1, col=4)
-fig.add_shape(type="rect", x0=1.31, x1=2.05, y0=0, y1=df["Depth (m)"].max(), fillcolor="LightSkyBlue", opacity=0.2,
-                layer="below", row=1, col=4)
-fig.add_shape(type="rect", x0=2.05, x1=2.6, y0=0, y1=df["Depth (m)"].max(), fillcolor="limegreen", opacity=0.2,
-                layer="below", row=1, col=4)
-fig.add_shape(type="rect", x0=2.6, x1=2.95, y0=0, y1=df["Depth (m)"].max(), fillcolor="tomato", opacity=0.2,
-                layer="below", row=1, col=4)
-fig.add_shape(type="rect", x0=2.95, x1=3.6, y0=0, y1=df["Depth (m)"].max(), fillcolor="gold", opacity=0.2,
-                layer="below", row=1, col=4)
 
-# Plot data
-fig.add_trace(go.Scatter(x=df['Qt'], y=df['Depth (m)'], name="Qt", marker=dict(size=20, color="CornflowerBlue")), row=1, col=1)
-fig.add_trace(go.Scatter(x=df['Fr'], y=df['Depth (m)'], name="Fr", marker=dict(size=20, color="MediumSeaGreen")), row=1, col=2)
-fig.add_trace(go.Scatter(x=df['Bq'], y=df['Depth (m)'], name="Bq", marker=dict(size=20, color="Slateblue")), row=1, col=3)
-fig.add_trace(go.Scatter(x=df['Ic'], y=df['Depth (m)'], name="Ic", marker=dict(size=20, color="Goldenrod")), row=1, col=4)
+    bh = st.sidebar.selectbox("Borehole", raw_df["BH"].unique())
 
-# Plot layer boundaries
-for col in range(4):
-    for layer in layers:
-        if params[col] == "Ic":
-            xmax, xmin = 0, 3.6
+    df = raw_df[raw_df["BH"] == bh]
+
+    num_layers = st.sidebar.number_input("Number of layers:", min_value=1, max_value=10, value=3, step=1)
+
+    layers = []
+    for i in range(num_layers - 1):
+        # User input (e.g., depth range selection)
+        depth = st.sidebar.slider(f"Layer boundary {i+1}", 0.0, df["Depth (m)"].max(), 1.0, step=0.1, format="%0.1f", key=i)
+        layers.append(depth)
+
+    @st.dialog("Input your name:")
+    def save_data():
+        user_name = st.text_input("Enter your name:")
+        #if st.button("Submit"):
+        if user_name:
+            # Build dataframe
+            layer = layers.sort()    # Make sure the layers are in order
+            save_df = pd.DataFrame(layers, columns=[bh])
+            csv_data = convert_for_download(save_df)
+            download = st.download_button("Download CSV", data=csv_data, file_name=f"{bh}_cpt layering_{user_name}.csv", mime="text/csv")
+            if download:
+                #log_event(f"Data downloaded : {bh} {user_name}")
+                st.success(f"Data downloaded :)")
         else:
-            xmax, xmin = df[params[col]].min(), df[params[col]].max()
-        fig.add_trace((go.Scatter(x=[xmin, xmax], y=[layer, layer], mode='lines',
-                                            line=dict(color='red', width=1, dash="dash"))) , row=1, col=col+1)
+            st.error("Please enter your name.")
 
-fig.update_yaxes(autorange="reversed")  # Depth increases downward
-fig.update_layout(width=1000, height=800, showlegend=False, margin=dict(t=50), yaxis_title="Depth (m)",
-         annotations=[dict(text=ann["text"], font=dict(size=20)) for ann in fig['layout']['annotations']])
-st.plotly_chart(fig, use_container_width=True)
+    # User clicks Save Button
+    if st.sidebar.button("Save Data"):
+        save_data()
+
+    params = ["Qt", "Fr", "Bq", "Ic"]
+
+    fig = make_subplots(
+    rows=1, cols=4,          # Number of rows and columns
+    shared_yaxes=True,       # Share x-axis for all subplots
+    vertical_spacing=0.1,    # Spacing between rows
+    subplot_titles=(["Qt [kPa]", "Fr [-]", "Bq [-]", "Ic [-]"])  # Titles for each subplot
+    )
+
+    # Plot Ic zones
+    fig.add_shape(type="rect", x0=0, x1=1.31, y0=0, y1=df["Depth (m)"].max(), fillcolor="crimson", opacity=0.2,
+                    layer="below", row=1, col=4)
+    fig.add_shape(type="rect", x0=1.31, x1=2.05, y0=0, y1=df["Depth (m)"].max(), fillcolor="LightSkyBlue", opacity=0.2,
+                    layer="below", row=1, col=4)
+    fig.add_shape(type="rect", x0=2.05, x1=2.6, y0=0, y1=df["Depth (m)"].max(), fillcolor="limegreen", opacity=0.2,
+                    layer="below", row=1, col=4)
+    fig.add_shape(type="rect", x0=2.6, x1=2.95, y0=0, y1=df["Depth (m)"].max(), fillcolor="tomato", opacity=0.2,
+                    layer="below", row=1, col=4)
+    fig.add_shape(type="rect", x0=2.95, x1=3.6, y0=0, y1=df["Depth (m)"].max(), fillcolor="gold", opacity=0.2,
+                    layer="below", row=1, col=4)
+
+    # Plot data
+    fig.add_trace(go.Scatter(x=df['Qt'], y=df['Depth (m)'], name="Qt", marker=dict(size=20, color="CornflowerBlue")), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['Fr'], y=df['Depth (m)'], name="Fr", marker=dict(size=20, color="MediumSeaGreen")), row=1, col=2)
+    fig.add_trace(go.Scatter(x=df['Bq'], y=df['Depth (m)'], name="Bq", marker=dict(size=20, color="Slateblue")), row=1, col=3)
+    fig.add_trace(go.Scatter(x=df['Ic'], y=df['Depth (m)'], name="Ic", marker=dict(size=20, color="Goldenrod")), row=1, col=4)
+
+    # Plot layer boundaries
+    for col in range(4):
+        for layer in layers:
+            if params[col] == "Ic":
+                xmax, xmin = 0, 3.6
+            else:
+                xmax, xmin = df[params[col]].min(), df[params[col]].max()
+            fig.add_trace((go.Scatter(x=[xmin, xmax], y=[layer, layer], mode='lines',
+                                                line=dict(color='red', width=1, dash="dash"))) , row=1, col=col+1)
+
+    fig.update_yaxes(autorange="reversed")  # Depth increases downward
+    fig.update_layout(width=1000, height=800, showlegend=False, margin=dict(t=50), yaxis_title="Depth (m)",
+            annotations=[dict(text=ann["text"], font=dict(size=20)) for ann in fig['layout']['annotations']])
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab2:
+    @st.cache_data
+    def load_plot_df():
+        plot_df = pd.read_excel("training data/EnBW cpt parameters_3d plot.xlsx")
+        return plot_df
+
+    @st.cache_data
+    def threeD_plot(plot_df):
+        trace = go.Scatter3d(
+        x=plot_df['East'],
+        y=plot_df['North'],
+        z=plot_df['Depth (m)'],
+        mode='markers',
+        marker=dict(
+            size=plot_df["Fr scaled"]*20+7,  # Marker size scaled by F_r
+            color=plot_df['Qt'],  # Color by Q_t
+            colorscale='Viridis',  # Color scale for Q_t
+            colorbar=dict(title='Qt'),
+            opacity=0.8,
+            cmin=0,
+            cmax=50,
+            symbol="circle",
+            line=dict(width=0)
+        )
+    )
+
+        layout = go.Layout(
+            scene=dict(
+                xaxis_title='X',
+                yaxis_title='Y',
+                zaxis_title='Depth (m)',
+                aspectratio=dict(x=1, y=1, z=1),
+                zaxis=dict(autorange='reversed')
+            ),
+            margin=dict(
+                l=50,  # Left margin
+                r=50,  # Right margin
+                b=50,  # Bottom margin
+                t=50),   # Top margin
+                width=800,  # Set the width of the plot
+                height=800,  # Set the height of the plot
+                title='3D Spatial Visualization of CPT Data'
+        )
+
+        fig = go.Figure(data=[trace], layout=layout)
+        return fig
+    
+    plot_df = load_plot_df()
+    fig = threeD_plot(plot_df)
+    st.plotly_chart(fig, use_container_width=True)
